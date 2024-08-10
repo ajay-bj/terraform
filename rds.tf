f06b45b63049f2c4a0994449f32db6840a078143
AJAYBALAJI969 <ajaybj969@gmail.com>
2024-08-10 11:17:40 +0530

    my seconf terraform commit

--- rds.tf
+++ rds.tf
@@ -4,9 +4,9 @@ resource "aws_db_instance" "default" {
   engine               = "mysql"
   engine_version       = "5.7"
   instance_class       = "db.t2.micro"
-  name                 = "mydb"
+  identifier           = "mydb"  # Use 'identifier' instead of 'name'
   username             = "foo"
   password             = file("../rds_pass.txt")
   parameter_group_name = "default.mysql5.7"
-  skip_final_snapshot = "true"
+  skip_final_snapshot  = true  # Use boolean value, not a string
 }
